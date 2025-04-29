a1eb202f8e0247865da38b4790c44c0556f1d055
Nick Maximillian <nick_maximillian@hotmail.com>
2025-04-29 13:12:22 +0200

    remove sub and tenant id

--- keyvault.tf
+++ keyvault.tf
@@ -11,41 +11,32 @@ terraform {
   }
 }
 
-# Get Azure authentication info (subscription_id, tenant_id)
-data "azurerm_client_config" "this" {}
-
 provider "azurerm" {
   features {}
-
-  subscription_id = data.azurerm_client_config.this.subscription_id
-  tenant_id       = data.azurerm_client_config.this.tenant_id
 }
 
-# Get a list of regions
+data "azurerm_client_config" "this" {}
+
 module "regions" {
   source  = "Azure/avm-utl-regions/azurerm"
   version = "0.3.0"
 }
 
-# Pick a random region (not used here but still initialized)
 resource "random_integer" "region_index" {
   max = length(module.regions.regions) - 1
   min = 0
 }
 
-# Create a random-compliant resource name helper
 module "naming" {
   source  = "Azure/naming/azurerm"
   version = "0.3.0"
 }
 
-# Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-avm-test-001"
-  location = "westeurope" # or module.regions.regions[random_integer.region_index.result] if you want randomized
+  location = "westeurope"
 }
 
-# Deploy Key Vault using the AVM module pulled from ACR
 module "keyvault" {
   source = "./modules/avm-res-keyvault-vault"
 
