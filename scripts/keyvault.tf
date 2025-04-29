bac4acc17edc129c92b3cf77be6ec87ccee41fa4
Nick Maximillian <nick_maximillian@hotmail.com>
2025-04-29 10:48:49 +0200

    test kv deployment

---
+++ scripts/keyvault.tf
@@ -0,0 +1,56 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_version = ">= 1.9, < 2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.117"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+# We need the tenant id for the key vault.
+data "azurerm_client_config" "this" {}
+
+# This allows us to randomize the region for the resource group.
+module "regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "0.3.0"
+}
+
+# This allows us to randomize the region for the resource group.
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-avm-test-001"
+  location = "westeurope" # Change to your preferred region
+}
+
+# This is the module call
+module "keyvault" {
+  source  = "vmmodulestf.azurecr.io/avm-res-keyvault-vault/azurerm"
+  version = "0.1.0"
+  # source              = "Azure/avm-res-keyvault-vault/azurerm"
+  name                = "zstestkv0101001"
+  enable_telemetry    = true
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  tenant_id           = data.azurerm_client_config.this.tenant_id
+}
